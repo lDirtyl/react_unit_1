e3de068923e9ac4e60c4e59ea3fd89d3cb79f874
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-11-24 01:26:58 +0100

    Button change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,15 @@
 import css from "./Button.module.css";
 import clsx from "clsx";
 
-const Button = ({ children, variant, size }) => {
+const Button = ({ children, variant, size, type }) => {
   return (
     <button
       className={clsx(
         css.btn,
         css.md,
         variant === "secondary" && css.primary,
-        size === "lg" && css.lg
+        size === "lg" && css.lg,
+        css.icon && type === "icon"
       )}
     >
       {children}
