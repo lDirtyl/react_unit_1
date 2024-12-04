da8d63a4f28572719cf07301ffce5ba863f4667f
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-04 22:01:36 +0100

    hoock

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 function App() {
   //Button
-  const handleClick = (e) => {
-    console.log(e);
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
+
+  // let value = 10;
+  const handleClick = () => {
+    setValue(value + 1);
   };
+  const handleClick2 = () => {
+    setValue2(value2 + 1);
+  };
+
   return (
     <>
-      <h1>qwerty</h1>
-      <button onClick={handleClick}>Click me!</button>
+      <h1>{value}</h1>
+      <button onClick={handleClick}>Click</button>
+      <h1>{value2}</h1>
+      <button onClick={handleClick2}>Click</button>
     </>
   );
 }
