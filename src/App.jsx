f8362672be761399b463d614fd868a7957695730
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-08 16:58:05 +0100

    pause

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import Timer from "./components/Timer/Timer.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  const [click, setClick] = useState(0);
+  const [click2, setClick2] = useState(0);
+  const [isShow, setIsShow] = useState(false);
+
+  //Mount
+  useEffect(() => {
+    console.log("effect");
+  }, []);
+
+  // Mount + Update
+  useEffect(() => {
+    console.log("click", click);
+  }, [click]);
+
+  // Update
+  useEffect(() => {
+    if (click2 === 0) return;
+    console.log("click2", click2);
+  }, [click2]);
+
+  console.log("outsideClick");
+
+  return (
+    <div>
+      <button onClick={() => setClick(click + 1)}>{click}</button>
+      <button onClick={() => setClick2(click2 + 1)}>{click2}</button>
+      <br />
+      <button onClick={() => setIsShow(!isShow)}>click</button>
+      {isShow && <Timer />}
+    </div>
+  );
 };
 
 export default App;
