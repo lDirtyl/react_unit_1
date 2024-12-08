3701f758f0cf6e838ea8a8d4d8adca5bb5c3e277
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-08 02:00:15 +0100

    prepair

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,5 @@
-import { useState } from "react";
-import data from "../posts.json";
-
-import Paginator from "./components/Paginator/Paginator";
-import Poster from "./components/Poster/Poster";
-
 const App = () => {
-  const [currentPost, setCurrentPost] = useState(1);
-
-  const postsCount = data.length;
-  const post = data[currentPost - 1];
-
-  const handlePrev = () => {
-    if (currentPost === 1) return;
-    setCurrentPost(currentPost - 1);
-  };
-
-  const handleNext = () => {
-    if (currentPost === postsCount) return;
-    setCurrentPost(currentPost + 1);
-  };
-
-  return (
-    <>
-      <Paginator
-        postCount={postsCount}
-        handlePrev={handlePrev}
-        handleNext={handleNext}
-        currentPost={currentPost}
-        postsCount={postsCount}
-      />
-
-      <hr />
-      <hr />
-      <Poster post={post} />
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
