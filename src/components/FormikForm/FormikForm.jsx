89552ccbb5cac0a46d19273665fba00303ad581c
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-06 14:35:47 +0100

    Formik finish

--- src/components/FormikForm/FormikForm.jsx
+++ src/components/FormikForm/FormikForm.jsx
@@ -1,11 +1,20 @@
-import { Formik, Form, Field } from 'formik';
-import css from './FormikForm.module.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import css from './FormikForm.module.css';
+import * as Yup from 'yup';
 
-const FormikForm = ({toSubmit}) => {
-//   const firstNameId = use
-
+const FormikForm = ({ toSubmit }) => {
+  const userSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(3, 'min 3')
+      .max(10, 'max 10')
+      .required('required'),
+    lastName: Yup.string()
+      .min(3, 'min 3')
+      .max(10, 'max 10')
+      .required('required'),
+  });
   const handleSubmit = (values, actions) => {
-    toSubmit(values)
+    toSubmit(values);
     actions.resetForm();
   };
   return (
@@ -13,19 +22,31 @@ const FormikForm = ({toSubmit}) => {
       initialValues={{
         firstName: '',
         lastName: '',
+        lang: 'de',
       }}
       onSubmit={handleSubmit}
+      validationSchema={userSchema}
     >
       <Form className={css.flex}>
         <label>
           firstNameId
           <Field type='text' name='firstName' />
+          <ErrorMessage name='firstName' />
         </label>
 
         <br />
         <label>
           lastNameId
           <Field type='text' name='lastName' />
+          <ErrorMessage name='lastName' />
+        </label>
+        <label>
+          <Field as='select' name='lang'>
+            <option value='ua'>UA</option>
+            <option value='de'>DE</option>
+            <option value='en'>EN</option>
+          </Field>
+          <ErrorMessage name='lang' />
         </label>
 
         <br />
