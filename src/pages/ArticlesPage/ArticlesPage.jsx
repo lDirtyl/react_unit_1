52da13e8e9a2bb74a6c025dc9610c33ba0b5a23f
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-11 11:35:55 +0100

    last changes

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -2,9 +2,16 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { getArticles } from './../../Api/Articles';
 import ArticleList from './../../components/ArticleList/ArticleList';
+import { useSearchParams } from 'react-router-dom';
 
 const ArticlesPage = () => {
   const [articles, setArticles] = useState(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterValue = searchParams.get('filter') ?? '';
+
+  // const [filterValue, setFilterValue] = useState('');
 
   useEffect(() => {
     const fetching = async () => {
@@ -18,10 +25,28 @@ const ArticlesPage = () => {
     fetching();
   }, []);
 
+  const handleChange = ({ target: { value } }) => {
+    if (value) {
+      searchParams.set('filter', value);
+      setSearchParams(searchParams);
+    } else {
+      searchParams.delete('filter');
+      setSearchParams(searchParams);
+    }
+  };
+
+  const filteredArticles = articles?.filter(el =>
+    el.author.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Articles Page</h1>
-      {articles && <ArticleList hits={articles} />}
+      <br />
+      Filter by author:
+      <input type='text' value={filterValue} onChange={handleChange} />
+      <br />
+      {filteredArticles && <ArticleList hits={filteredArticles} />}
     </div>
   );
 };
